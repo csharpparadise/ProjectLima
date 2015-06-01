aea9af63a2f3cd2739aa876d123da6cdc9ac2787
Thomas Klepzig <thomas.klepzig@comparex.de>
2015-06-01 13:55:16 +0200

    added UI skeleton

---
+++ source/UI/server.js
@@ -0,0 +1,18 @@
+var port = 49000;
+var express = require("express");
+var app = express();
+var http = require("http").Server(app);
+var path = require("path");
+
+app.use("/", express.static(path.resolve(__dirname + "/public")))
+
+app.get("/*", function(request, response)
+{
+    response.sendFile(path.resolve(__dirname + "/public/index.html"));
+});
+
+
+http.listen(port, function()
+{
+    console.log("listening on *:" + port);
+});
