01f178fec318d6eee94f5a6d7e4025e1e2cd27f0
Alexander Tank <alexander.tank@comparex.de>
2015-06-01 15:14:40 +0200

    Core Interface + Ws Interface definiert #3

---
+++ source/Lima.Core/IBoardService.cs
@@ -0,0 +1,32 @@
+﻿namespace Lima.Core
+{
+    public interface IBoardService
+    {
+        void SpalteAnlegen(Spalte spalte);
+        void KarteAnlegen(Karte karte, int spalteId);
+        Board BoardHolen();
+    }
+
+    public class BoardService : IBoardService
+    {
+        public BoardService()
+        {
+            
+        }
+
+        public void SpalteAnlegen(Spalte spalte)
+        {
+            
+        }
+
+        public void KarteAnlegen(Karte karte, int spalteId)
+        {
+            
+        }
+
+        public Board BoardHolen()
+        {
+            return new Board();
+        }
+    }
+}
