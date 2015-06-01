c32041e2bb2800b2671d4c97a0f799295213c5cb
Alexander Tank <alexander.tank@comparex.de>
2015-06-01 15:13:54 +0200

    Core Interface + WS Interface definiert

---
+++ source/Lima.api/Controllers/KarteController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+using Lima.Core;
+
+namespace Lima.Api.Controllers
+{
+    public class KarteController : ApiController
+    {
+        private readonly IBoardService _boardService;
+
+        public KarteController(IBoardService boardService)
+        {
+            _boardService = boardService;
+        }
+
+        public void Post([FromBody]Karte karte, int spalteId)
+        {
+            _boardService.KarteAnlegen(karte, spalteId);
+        }
+    }
+}
---
+++ source/Lima.api/Controllers/SpalteController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+using Lima.Core;
+
+namespace Lima.Api.Controllers
+{
+    public class SpalteController : ApiController
+    {
+        private readonly IBoardService _boardService;
+
+        public SpalteController(IBoardService boardService)
+        {
+            _boardService = boardService;
+        }
+
+        public void Post([FromBody]Spalte spalte)
+        {
+            _boardService.SpalteAnlegen(spalte);
+        }
+    }
+}
