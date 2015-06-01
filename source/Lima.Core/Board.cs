56e484f9b8b3c75114821a4129e7eb4ebac698d2
Alexander Tank <alexander.tank@comparex.de>
2015-06-01 13:27:20 +0200

    Lima.Core angelegt und Domain Objekte hinzugefügt

---
+++ source/Lima.Core/Board.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Lima.Core
+{
+    public class Board
+    {
+        public Board()
+        {
+            Spalten = new List<Spalte>();
+        }
+        public List<Spalte> Spalten { get; set; }
+    }
+}
---
+++ source/Lima.Core/Spalte.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Lima.Core
+{
+    public class Spalte
+    {
+        public Spalte()
+        {
+            Karten = new List<Karte>();
+        }
+        public string Titel { get; set; }
+        public List<Karte> Karten { get; set; }
+    }
+}
