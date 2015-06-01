018e5b75cebeedc6cf78e733d9ed3e2dba4862fe
Alexander Tank <alexander.tank@comparex.de>
2015-06-01 16:27:28 +0200

    Datenbankanbindung implementiert

---
+++ source/Lima.Core.IntegrationTests/BoardServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using Lima.Data;
+using NUnit.Framework;
+using Should;
+
+namespace Lima.Core.IntegrationTests
+{
+    [TestFixture]
+    public class BoardServiceTests
+    {
+        [Test]
+        public void Stelle_sicher_Board_ist_vorhanden()
+        {
+            var boardService = new BoardService(new LimaDbContext());
+
+            var board = boardService.BoardHolen();
+
+            board.ShouldNotBeNull();
+        }
+    }
+}
---
+++ source/Lima.Core/BoardService.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using Lima.Data;
+
+namespace Lima.Core
+{
+    public class BoardService : IBoardService
+    {
+        private readonly ILimaDbContext _dbContext;
+
+        public BoardService(ILimaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void SpalteAnlegen(Spalte spalte)
+        {
+            var board = _dbContext.Boards.Single();
+            
+            board.Spaltes.Add(new Data.Spalte { Titel = spalte.Titel});
+        }
+
+        public void KarteAnlegen(Karte karte, int spalteId)
+        {
+            var entity = new Data.Karte
+            {
+                SpalteId = spalteId,
+                Beschreibung = karte.Beschreibung,
+                Titel = karte.Titel
+            };
+            _dbContext.Kartes.Add(entity);
+
+            karte.Id = entity.Id;
+        }
+
+        public Board BoardHolen()
+        {
+            var board = _dbContext.Boards.Single();
+
+            return new Board
+            { 
+                Spalten = board.Spaltes.Select(x => new Spalte 
+                { 
+                    Titel = x.Titel, 
+                    Karten = x.Kartes.Select(k => new Karte
+                    {
+                        Titel = k.Titel, 
+                        Beschreibung = k.Beschreibung
+                    }).ToList()
+                }).ToList()
+            };
+        }
+    }
+}
