95c7ac50fbe6023efcfb7f0efc6c3c898962da57
Thomas <Thomas@Subnotebook>
2015-07-26 19:00:41 +0200

    added two more sample tests, fixed karma versions

--- source/UI/public/app/karte/karte.spec.js
+++ source/UI/public/app/karte/karte.spec.js
@@ -21,6 +21,17 @@ describe('karte', function() {
         });
     }));
 
+    describe('$scope', function() {
+        it('should have dummyValueForTesting with any value', function() {
+            expect($scope.dummyValueForTesting).toBeDefined();
+        });
+
+        it('should have dummyValueForTesting with the value 42', function() {
+            expect($scope.dummyValueForTesting).toBe(42);
+        });
+    });
+
+
     describe('openKarte', function() {
         it('should change location to /karte/:id', function() {
             spyOn($location, 'path');
