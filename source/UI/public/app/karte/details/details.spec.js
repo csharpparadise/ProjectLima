291510f587fb936176dfa75a8f8822c43c57614d
Andreas Gräfe <andreas.graefe@comparex.de>
2015-08-03 15:51:23 +0200

    testing mock of async/promised functions in karte.details controller

--- source/UI/public/app/karte/details/details.spec.js
+++ source/UI/public/app/karte/details/details.spec.js
@@ -1,31 +1,62 @@
 'use strict';
 
-describe('karte/details', function(){
+describe('karte/details', function() {
     var $scope;
     var kartenService;
+    var $routeParams;
+    var createCrtl;
+    var $q;
 
     beforeEach(function() {
         angular.mock.module('ngRoute', 'kartenService');
         module('karte.details');
     });
 
-    beforeEach(inject(function(_$rootScope_, _$controller_, _kartenService_) {
+    beforeEach(inject(function(_$rootScope_, _$controller_, _$routeParams_, _kartenService_, _$q_) {
         $scope = _$rootScope_.$new();
         kartenService = _kartenService_;
+        $routeParams = _$routeParams_;
+        $q = _$q_;
 
-        _$controller_('DetailsController', {
-            $scope: $scope
-        });
+        createCrtl = function() {
+            return _$controller_('DetailsController', {
+                $scope: $scope
+            });
+        };
     }));
 
     describe('$scope', function() {
         it('should not call saveKarte when no Karte is given', function() {
+            createCrtl();
+
             spyOn(kartenService, 'saveKarte');
-
             $scope.changeCard();
-
             expect(kartenService.saveKarte).not.toHaveBeenCalled();
+        });
+
+        it('should scope have karteid after init', function() {
+            $routeParams.karteId = 42;
+
+            createCrtl();
+            expect($scope.karteId).toBe(42);
+        })
+
+        it('should have karte after init', function() {
+            // var deferred = $q.defer();
+            // spyOn(kartenService, 'getKarteById').and.returnValue(deferred.promise);
+            // deferred.resolve(21);
+
+            spyOn(kartenService, 'getKarteById').and.callFake(function(id) {
+                var deferred = $q.defer();
+                deferred.resolve(21);
+                return deferred.promise;
+            });
+
+            console.log(jasmine.version);
+            createCrtl();
+            $scope.$digest();
+            expect($scope.karte).toBeDefined();
+            expect($scope.karte).toBe(21);
         })
     });
-
 })
